8ba77c42b0d44f52bdeec3bfc95875c8450cbdb9
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-01-20 19:24:27 -0500

    Added user model client js

---
+++ webdev-sp19-server-java-login/src/main/webapp/models/user.model.service.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+
+function User(username, password, firstName, lastName) {
+  this.username = username;
+  this.password = password;
+  this.lastName = lastName;
+  this.firstName = firstName;
+ 
+  this.setUsername = setUsername;
+  this.getUsername = getUsername;
+  
+  this.setPassword = setPassword;
+  this.getPassword = getPassword;
+  
+  this.setLastName = setLastName;
+  this.getLastName = getLastName;
+  
+  this.setFirstName = setFirstName;
+  this.getFirstName = getFirstName;
+  
+  
+  function setUsername(username) {
+    this.username = username;
+  }
+  function getUsername() {
+    return this.username;
+  }
+  
+  
+  function setLastName(lastName) {
+	    this.lastName = lastName;
+  }
+  function getLastName() {
+	    return this.lastName;
+  }
+  
+  
+  
+  function setPassword(password) {
+	    this.password = password;
+	  }
+  function getPassword() {
+	   return this.password;
+  }
+  
+	
+  function setFirstName(firstName) {
+	    this.firstName = firstName;
+	  }
+  function getFirstName() {
+	    return this.firstName;
+   }
+  
+	  
+	  
